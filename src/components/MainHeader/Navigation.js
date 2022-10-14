20e636639cd9a3b18c2f0f201583ed710b8081f7
maheshlnt <105842380+maheshlnt@users.noreply.github.com>
2022-10-14 12:18:41 +0530

    Update Navigation.js

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,3 +1,46 @@
+---------------------------------------------CODE 1 -----------with usecontext hook---------------------------------------------
+import React, {useContext} from 'react';
+import AuthContext from '../../store/auth-context'; 
+
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+ const ctx=  useContext(AuthContext);  //changes: newly added this and imported useContext
+  return (
+    // <AuthContext.Consumer>   //Removed these 3 lines
+    //   {(ctx)=>{
+    //     return (
+        <nav className={classes.nav}>
+          <ul>
+            {ctx.isLoggedIn && (
+              <li>
+                <a href="/">Users</a>
+              </li>
+            )}
+            {/* {props.isLoggedIn && ( */}
+            {ctx.isLoggedIn && (
+              <li>
+                <a href="/">Admin</a>
+              </li>
+            )}
+            {ctx.isLoggedIn && (
+              <li>
+                <button onClick={props.onLogout}>Logout</button>
+              </li>
+            )}
+          </ul>
+        </nav>
+    //     )
+    //   }}  
+    //   {/* will get data of auth context comp */}
+    
+    // </AuthContext.Consumer>
+  );
+};
+
+export default Navigation;
+
+----------------------------------------CODE 2 dont use--without usecontext hook-----------------------------------
 import React from 'react';
 import AuthContext from '../../store/auth-context';
 
